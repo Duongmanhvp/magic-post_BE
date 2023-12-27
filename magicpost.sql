2467fa2ebaa65aa52d0d0934e55d7f33066cc1dc
DuongManhMAC <macosx@192.168.0.3>
2023-12-27 13:19:14 +0700

    new db

--- magicpost.sql
+++ magicpost.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: localhost:3306
--- Thời gian đã tạo: Th10 28, 2023 lúc 04:37 PM
+-- Thời gian đã tạo: Th12 18, 2023 lúc 03:42 PM
 -- Phiên bản máy phục vụ: 10.4.27-MariaDB
 -- Phiên bản PHP: 8.1.12
 
@@ -29,10 +29,22 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `accounts` (
   `account_id` int(11) NOT NULL,
-  `acc_name` varchar(50) NOT NULL,
+  `name` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL,
-  `roles` int(11) NOT NULL
+  `roles` int(11) NOT NULL,
+  `phone` varchar(50) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `accounts`
+--
+
+INSERT INTO `accounts` (`account_id`, `name`, `password`, `roles`, `phone`, `email`) VALUES
+(1, 'duongmanh', '123456', 0, NULL, NULL),
+(2, 'caodung', '123456', 0, NULL, NULL),
+(3, 'trantoan', '123456', 0, NULL, NULL),
+(4, 'customer', '123456', 3, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -42,10 +54,19 @@ CREATE TABLE `accounts` (
 
 CREATE TABLE `customers` (
   `customer_id` int(11) NOT NULL,
-  `firstName` varchar(50) DEFAULT NULL,
-  `lastName` varchar(50) NOT NULL,
+  `firstname` varchar(50) DEFAULT NULL,
+  `lastname` varchar(50) NOT NULL,
+  `phone` varchar(50) DEFAULT NULL,
+  `address` varchar(1000) DEFAULT NULL,
   `account_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `customers`
+--
+
+INSERT INTO `customers` (`customer_id`, `firstname`, `lastname`, `phone`, `address`, `account_id`) VALUES
+(1, 'Tran', 'Toan', '0368432907', NULL, 4);
 
 -- --------------------------------------------------------
 
@@ -55,8 +76,8 @@ CREATE TABLE `customers` (
 
 CREATE TABLE `employee` (
   `employee_id` int(11) NOT NULL,
-  `firstName` varchar(50) DEFAULT NULL,
-  `lastName` varchar(50) NOT NULL,
+  `firstname` varchar(50) DEFAULT NULL,
+  `lastname` varchar(50) NOT NULL,
   `account_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -68,7 +89,7 @@ CREATE TABLE `employee` (
 
 CREATE TABLE `locations` (
   `location_id` int(11) NOT NULL,
-  `address` varchar(50) NOT NULL,
+  `address` varchar(1000) NOT NULL,
   `type` varchar(50) NOT NULL,
   `account_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -83,8 +104,9 @@ CREATE TABLE `orders` (
   `order_id` int(11) NOT NULL,
   `order_date` date NOT NULL,
   `status` varchar(50) NOT NULL,
-  `location1` int(11) NOT NULL,
-  `location2` int(11) NOT NULL
+  `sending_location` int(11) NOT NULL,
+  `reciving_location` int(11) NOT NULL,
+  `parcel_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -96,8 +118,18 @@ CREATE TABLE `orders` (
 CREATE TABLE `parcels` (
   `parcel_id` int(11) NOT NULL,
   `record_date` date NOT NULL,
-  `customer_id` int(11) NOT NULL
+  `customer_id` int(11) NOT NULL,
+  `parcel_code` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `parcels`
+--
+
+INSERT INTO `parcels` (`parcel_id`, `record_date`, `customer_id`, `parcel_code`) VALUES
+(1, '2023-09-09', 1, 'MP2023TT123'),
+(2, '2023-09-09', 1, 'MP2023TT123VN'),
+(3, '2023-10-10', 1, 'MP2023VN432XX');
 
 --
 -- Chỉ mục cho các bảng đã đổ
@@ -128,15 +160,16 @@ ALTER TABLE `employee`
 --
 ALTER TABLE `locations`
   ADD PRIMARY KEY (`location_id`),
-  ADD KEY `account_id` (`account_id`);
+  ADD KEY `locations_ibfk_1` (`account_id`);
 
 --
 -- Chỉ mục cho bảng `orders`
 --
 ALTER TABLE `orders`
   ADD PRIMARY KEY (`order_id`),
-  ADD KEY `location1` (`location1`),
-  ADD KEY `location2` (`location2`);
+  ADD KEY `sending_location` (`sending_location`),
+  ADD KEY `reciving_location` (`reciving_location`),
+  ADD KEY `fk_parcel_constraint` (`parcel_id`);
 
 --
 -- Chỉ mục cho bảng `parcels`
@@ -144,6 +177,46 @@ ALTER TABLE `orders`
 ALTER TABLE `parcels`
   ADD PRIMARY KEY (`parcel_id`),
   ADD KEY `customer_id` (`customer_id`);
+
+--
+-- AUTO_INCREMENT cho các bảng đã đổ
+--
+
+--
+-- AUTO_INCREMENT cho bảng `accounts`
+--
+ALTER TABLE `accounts`
+  MODIFY `account_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT cho bảng `customers`
+--
+ALTER TABLE `customers`
+  MODIFY `customer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT cho bảng `employee`
+--
+ALTER TABLE `employee`
+  MODIFY `employee_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `locations`
+--
+ALTER TABLE `locations`
+  MODIFY `location_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `parcels`
+--
+ALTER TABLE `parcels`
+  MODIFY `parcel_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Các ràng buộc cho các bảng đã đổ
@@ -171,8 +244,9 @@ ALTER TABLE `locations`
 -- Các ràng buộc cho bảng `orders`
 --
 ALTER TABLE `orders`
-  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`location1`) REFERENCES `locations` (`location_id`),
-  ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`location2`) REFERENCES `locations` (`location_id`);
+  ADD CONSTRAINT `fk_parcel_constraint` FOREIGN KEY (`parcel_id`) REFERENCES `parcels` (`parcel_id`),
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`sending_location`) REFERENCES `locations` (`location_id`),
+  ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`reciving_location`) REFERENCES `locations` (`location_id`);
 
 --
 -- Các ràng buộc cho bảng `parcels`
